71225015772d36ec757a07d8b49af4374ad15415
ravila10 <ravila10@cuc.edu.co>
2024-09-24 12:47:46 -0500

    m1

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import MiLogo from "../public/Ronaldo 2008.jpg";
+function App() {
+  return (
+    <>
+      <div>
+        <img src={MiLogo} alt="" />
+        <h1>the red devil</h1>
+        <p>El mejor prime de la historia del futbol</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
